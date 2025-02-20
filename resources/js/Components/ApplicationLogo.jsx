93b6257d283ba3f34c5be615db0ee918a5baadef
Fabio Rodrigues <fabio.rod@outlook.pt>
2025-02-20 14:49:23 +0000

    chore: fix font family load

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -4,7 +4,7 @@ export default function ApplicationLogo() {
             <a className="flex" href={route('home')}>
                 <div className="flex items-center gap-2">
                     <div className="me-2 justify-center rounded-lg bg-blue-500 px-2 py-0 text-2xl font-bold">V</div>
-                    <div className="font-medium dark:text-white">
+                    <div className="font-medium text-gray-900 dark:text-white">
                         <div>Vertical</div>
                     </div>
                 </div>
