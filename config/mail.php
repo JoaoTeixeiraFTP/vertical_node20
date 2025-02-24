2592e081fd083d5ca43742ded5ab1aac1b64be83
Joao Teixeira <joao.teixeira@ftpporto.com>
2025-02-24 15:13:08 +0000

    filesystems também database também mail

--- config/mail.php
+++ config/mail.php
@@ -37,17 +37,7 @@ return [
 
     'mailers' => [
 
-        'smtp' => [
-            'transport' => 'smtp',
-            'scheme' => env('MAIL_SCHEME'),
-            'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', '127.0.0.1'),
-            'port' => env('MAIL_PORT', 2525),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
-        ],
+
 
         'ses' => [
             'transport' => 'ses',
@@ -63,16 +53,6 @@ return [
 
         'resend' => [
             'transport' => 'resend',
-        ],
-
-        'sendmail' => [
-            'transport' => 'sendmail',
-            'path' => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -bs -i'),
-        ],
-
-        'log' => [
-            'transport' => 'log',
-            'channel' => env('MAIL_LOG_CHANNEL'),
         ],
 
         'array' => [
