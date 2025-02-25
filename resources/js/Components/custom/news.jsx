e905aa22b1ac5fbc73bdd9070eac9058a01d672e
Fabio Rodrigues <fabio.rod@outlook.pt>
2025-02-25 15:46:07 +0000

    feat: alteraçao no component news.jsx

--- resources/js/Components/custom/news.jsx
+++ resources/js/Components/custom/news.jsx
@@ -1,3 +1,4 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/Components/ui/card.jsx';
 import { useCallback, useEffect, useState } from 'react';
 import placeholder from '../../../../public/images/placeholder.jpg';
 
@@ -5,10 +6,9 @@ export default function News({ result }) {
     const [businessIndex, setBusinessIndex] = useState(1);
     const [techCrunchIndex, setTechCrunchIndex] = useState(0);
 
-    const businessNews = result.businessNews?.length ? result.businessNews : [];
+    const businessNews = result.businessHeadlines?.length ? result.businessHeadlines : [];
     const techCrunchNews = result.techCrunchHeadlines?.length ? result.techCrunchHeadlines : [];
 
-    const isLoading = businessNews.length === 0 && techCrunchNews.length === 0;
     const isFullWidth = (businessNews.length > 0 && techCrunchNews.length === 0) || (businessNews.length === 0 && techCrunchNews.length > 0);
 
     const updateBusinessIndex = useCallback(() => {
@@ -33,73 +33,99 @@ export default function News({ result }) {
         }
     }, [techCrunchNews.length, updateTechCrunchIndex]);
 
-    if (isLoading) {
-        return (
-            <div className="flex h-40 items-center justify-center rounded-xl bg-white shadow-sm dark:bg-gray-800">
-                <p className="text-lg text-gray-800 dark:text-gray-100">Carregando notícias...</p>
-            </div>
-        );
-    }
-
     return (
-        <div className={`flex flex-col ${isFullWidth ? 'w-full' : 'w-1/2'} rounded-xl bg-white p-5 shadow-sm dark:bg-gray-800`}>
-            <h2 className="mb-4 font-semibold text-gray-800 dark:text-gray-100">Notícias</h2>
-
-            {businessNews.length > 0 && (
-                <div className="mb-6">
-                    <h3 className="font-semibold text-gray-800 dark:text-gray-100">Business</h3>
+        <Card className={'block h-[30vh]'}>
+            <CardHeader>
+                <CardTitle>Business</CardTitle>
+                <hr className="mb-2 mt-2 border-t border-gray-200 dark:border-gray-700" />
+            </CardHeader>
+            <CardContent>
+                {' '}
+                {businessNews.length > 0 && (
                     <div className="overflow-hidden">
                         <div className="flex transition-all duration-500 ease-in-out">
                             {businessNews.map((news, index) => (
-                                <div key={index} className={`w-80 flex-shrink-0 ${index !== businessIndex && 'hidden'}`}>
-                                    <div className="flex items-center space-x-3 py-2">
-                                        <img src={news.img ?? placeholder} alt="Imagem" className="h-16 w-16 rounded-md object-cover" />
+                                <div key={index} className={`w-full flex-shrink-0 ${index !== businessIndex && 'hidden'}`}>
+                                    <div className="mb-4 flex items-center space-x-3 py-2">
+                                        <img src={news.urlToImage ?? placeholder} alt="Imagem" className="h-16 w-16 rounded-md object-cover" />
                                         <div>
-                                            <h3 className="w-52 break-words text-sm font-semibold text-gray-800 dark:text-gray-100">{news.title}</h3>
+                                            <h3 className="w-42 break-words text-sm font-semibold text-gray-800 dark:text-gray-100">{news.title}</h3>
+                                            <p className="text-xs text-gray-600 dark:text-gray-300">{news.date}</p>
+                                        </div>
+                                    </div>
+                                    <div className="flex items-center space-x-3 py-2">
+                                        <img src={news.urlToImage ?? placeholder} alt="Imagem" className="h-16 w-16 rounded-md object-cover" />
+                                        <div>
+                                            <h3 className="w-42 break-words text-sm font-semibold text-gray-800 dark:text-gray-100">{news.title}</h3>
                                             <p className="text-xs text-gray-600 dark:text-gray-300">{news.date}</p>
                                         </div>
                                     </div>
                                 </div>
                             ))}
                         </div>
-                    </div>
-                </div>
-            )}
-
-            {techCrunchNews.length > 0 && (
-                <div>
-                    <h3 className="font-semibold text-gray-800 dark:text-gray-100">TechCrunch</h3>
-                    <div className="overflow-hidden">
-                        <div className="flex transition-all duration-500 ease-in-out">
-                            {techCrunchNews.map((article, index) => (
-                                <div key={index} className={`w-80 flex-shrink-0 ${index !== techCrunchIndex && 'hidden'}`}>
-                                    <div className="flex items-center space-x-3 py-2">
-                                        <img src={article.img ?? placeholder} alt="Imagem" className="h-16 w-16 rounded-md object-cover" />
-                                        <div>
-                                            <h3 className="w-52 break-words text-sm font-semibold text-gray-800 dark:text-gray-100">
-                                                {article.title}
-                                            </h3>
-                                            <p className="text-xs text-gray-600 dark:text-gray-300">{article.date}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-
-                        {techCrunchNews.length > 1 && (
+                        {businessNews.length > 1 && (
                             <div className="mt-3 flex justify-center space-x-2">
-                                {techCrunchNews.map((_, index) => (
+                                {businessNews.map((_, index) => (
                                     <span
                                         key={index}
-                                        className={`h-2 w-2 cursor-pointer rounded-full ${index === techCrunchIndex ? 'bg-blue-800' : 'bg-gray-300'}`}
-                                        onClick={() => setTechCrunchIndex(index)}
+                                        className={`h-2 w-2 cursor-pointer rounded-full ${index === businessIndex ? 'bg-blue-800' : 'bg-gray-300'}`}
+                                        onClick={() => setBusinessIndex(index)}
                                     ></span>
                                 ))}
                             </div>
                         )}
                     </div>
-                </div>
-            )}
-        </div>
+                )}
+            </CardContent>
+            <CardHeader>
+                <CardTitle>TechCrunh</CardTitle>
+                <hr className="mb-2 mt-2 border-t border-gray-200 dark:border-gray-700" />
+            </CardHeader>
+            <CardContent>
+                {' '}
+                {techCrunchNews.length > 0 && (
+                    <div>
+                        <div className="overflow-hidden">
+                            <div className="row-span-2 grid transition-all duration-500 ease-in-out">
+                                {techCrunchNews.map((article, index) => (
+                                    <div key={index} className={`w-80 flex-shrink-0 ${index !== techCrunchIndex && 'hidden'}`}>
+                                        <div className="mb-4 flex items-center space-x-3 py-2">
+                                            <img src={article.urlToImage ?? placeholder} alt="Imagem" className="h-16 w-16 rounded-md object-cover" />
+                                            <div>
+                                                <h3 className="w-42 break-words text-sm font-semibold text-gray-800 dark:text-gray-100">
+                                                    {article.title}
+                                                </h3>
+                                                <p className="text-xs text-gray-600 dark:text-gray-300">{article.date}</p>
+                                            </div>
+                                        </div>
+                                        <div className="flex items-center space-x-3 py-2">
+                                            <img src={article.urlToImage ?? placeholder} alt="Imagem" className="h-16 w-16 rounded-md object-cover" />
+                                            <div>
+                                                <h3 className="w-52 break-words text-sm font-semibold text-gray-800 dark:text-gray-100">
+                                                    {article.title}
+                                                </h3>
+                                                <p className="text-wrap text-xs text-gray-600 dark:text-gray-300">{article.date}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+
+                            {techCrunchNews.length > 1 && (
+                                <div className="mt-3 flex justify-center space-x-2">
+                                    {techCrunchNews.map((_, index) => (
+                                        <span
+                                            key={index}
+                                            className={`h-2 w-2 cursor-pointer rounded-full ${index === techCrunchIndex ? 'bg-blue-800' : 'bg-gray-300'}`}
+                                            onClick={() => setTechCrunchIndex(index)}
+                                        ></span>
+                                    ))}
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                )}
+            </CardContent>
+        </Card>
     );
 }
