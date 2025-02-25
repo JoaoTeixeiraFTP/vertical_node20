3e48133766b35adbcb9fe211fb7b6a0061af71ad
Fabio Rodrigues <fabio.rod@outlook.pt>
2025-02-25 10:06:39 +0000

    fix: mudando de dashboard para home

--- resources/js/Pages/welcome.jsx
+++ resources/js/Pages/welcome.jsx
@@ -39,6 +39,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                                     </Link>
                                 ) : (
                                     <>
+                                        {/*TODO - mudar para nao ser hardcode demo*/}
                                         <Link
                                             href={route('login', 'demo')}
                                             className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
