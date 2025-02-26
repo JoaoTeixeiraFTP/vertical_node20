77705f9b9e17b01265556b76b9d3d82fd4e331b2
JoaoTeixeiraFTP <joao.teixeira@ftpporto.com>
2025-02-26 16:01:44 +0000

    descomentado

--- config/database.php
+++ config/database.php
@@ -88,8 +88,8 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'prefix' => '',
             'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
+            'encrypt' => env('DB_ENCRYPT', 'yes'),
+            'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'true'),
         ],
 
     ],
