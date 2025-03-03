44e0faf7288c10971feec7c9c3aa1a9fbd011214
JoaoTeixeiraFTP <joao.teixeira@ftpporto.com>
2025-03-03 08:31:30 +0000

    FilterButton

--- resources/js/Components/custom/FilterButton.jsx
+++ resources/js/Components/custom/FilterButton.jsx
@@ -38,7 +38,7 @@ function FilterButton({ align }) {
                     onClick={() => setDropdownOpen(!dropdownOpen)}
                     aria-expanded={dropdownOpen}
                 >
-                    <span className="sr-only">Filter</span>
+                    <span className="sr-only">Filtro</span>
                     <wbr />
                     <svg className="fill-current" width="16" height="16" viewBox="0 0 16 16">
                         <path d="M0 3a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H1a1 1 0 0 1-1-1ZM3 8a1 1 0 0 1 1-1h8a1 1 0 1 1 0 2H4a1 1 0 0 1-1-1ZM7 12a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2H7Z" />
@@ -47,7 +47,7 @@ function FilterButton({ align }) {
             </PopoverTrigger>
             <PopoverContent className={'m-0 p-0'}>
                 <div className="grid gap-4">
-                    <div className="px-3 pb-2 pt-1.5 text-xs font-semibold uppercase text-gray-400 dark:text-gray-500">Filters</div>
+                    <div className="px-3 pb-2 pt-1.5 text-xs font-semibold uppercase text-gray-400 dark:text-gray-500">Filtros</div>
                     <form className={'m-0 p-0'}>
                         <ul>
                             <li className="px-3 py-1">
@@ -77,7 +77,7 @@ function FilterButton({ align }) {
                             <li className="px-3 py-1">
                                 <label className="flex items-center">
                                     <input type="checkbox" className="form-checkbox" />
-                                    <span className="ml-2 text-sm font-medium">Last Order</span>
+                                    <span className="ml-2 text-sm font-medium">Ultimas Faturas</span>
                                 </label>
                             </li>
                             <li className="px-3 py-1">
@@ -91,7 +91,7 @@ function FilterButton({ align }) {
                             <ul className="flex items-center justify-between">
                                 <li>
                                     <button className="btn-xs border-gray-200 bg-white text-red-500 hover:border-gray-300 dark:border-gray-700/60 dark:bg-gray-800 dark:hover:border-gray-600">
-                                        Clear
+                                        Limpar
                                     </button>
                                 </li>
                                 <li>
@@ -100,7 +100,7 @@ function FilterButton({ align }) {
                                         onClick={() => setDropdownOpen(false)}
                                         onBlur={() => setDropdownOpen(false)}
                                     >
-                                        Apply
+                                        Aplicar
                                     </button>
                                 </li>
                             </ul>
