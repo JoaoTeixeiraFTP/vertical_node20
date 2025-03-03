9d740d2412602ff8b38cbf2a9edf803b8a98ac22
JoaoTeixeiraFTP <joao.teixeira@ftpporto.com>
2025-03-03 15:54:11 +0000

    Help

--- resources/js/Components/custom/Help.jsx
+++ resources/js/Components/custom/Help.jsx
@@ -38,7 +38,7 @@ export default function Help({ align }) {
                 onClick={() => setDropdownOpen(!dropdownOpen)}
                 aria-expanded={dropdownOpen}
             >
-                <span className="sr-only">Need help?</span>
+                <span className="sr-only">Ajuda?</span>
                 <svg
                     className="fill-current text-gray-500/80 dark:text-gray-400/80"
                     width={16}
@@ -62,7 +62,7 @@ export default function Help({ align }) {
                 leaveEnd="opacity-0"
             >
                 <div ref={dropdown} onFocus={() => setDropdownOpen(true)} onBlur={() => setDropdownOpen(false)}>
-                    <div className="px-3 pb-2 pt-1.5 text-xs font-semibold uppercase text-gray-400 dark:text-gray-500">Need help?</div>
+                    <div className="px-3 pb-2 pt-1.5 text-xs font-semibold uppercase text-gray-400 dark:text-gray-500">Ajuda?</div>
                     <ul>
                         <li>
                             <Link
@@ -74,7 +74,7 @@ export default function Help({ align }) {
                                     <rect y="3" width="12" height="9" rx="1" />
                                     <path d="M2 0h8v2H2z" />
                                 </svg>
-                                <span>Documentation</span>
+                                <span>Documentação</span>
                             </Link>
                         </li>
                         <li>
@@ -86,7 +86,7 @@ export default function Help({ align }) {
                                 <svg className="mr-2 h-3 w-3 shrink-0 fill-current text-violet-500" viewBox="0 0 12 12">
                                     <path d="M10.5 0h-9A1.5 1.5 0 000 1.5v9A1.5 1.5 0 001.5 12h9a1.5 1.5 0 001.5-1.5v-9A1.5 1.5 0 0010.5 0zM10 7L8.207 5.207l-3 3-1.414-1.414 3-3L5 2h5v5z" />
                                 </svg>
-                                <span>Support Site</span>
+                                <span>Suporte Site</span>
                             </Link>
                         </li>
                         <li>
@@ -98,7 +98,7 @@ export default function Help({ align }) {
                                 <svg className="mr-2 h-3 w-3 shrink-0 fill-current text-violet-500" viewBox="0 0 12 12">
                                     <path d="M11.854.146a.5.5 0 00-.525-.116l-11 4a.5.5 0 00-.015.934l4.8 1.921 1.921 4.8A.5.5 0 007.5 12h.008a.5.5 0 00.462-.329l4-11a.5.5 0 00-.116-.525z" />
                                 </svg>
-                                <span>Contact us</span>
+                                <span>Contactos</span>
                             </Link>
                         </li>
                     </ul>
