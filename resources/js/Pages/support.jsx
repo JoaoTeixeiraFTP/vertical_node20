94e00b7dd19422c56cd74eeedef603918798fd2b
JoaoTeixeiraFTP <joao.teixeira@ftpporto.com>
2025-03-21 11:38:46 +0000

    h

--- resources/js/Pages/support.jsx
+++ resources/js/Pages/support.jsx
@@ -36,8 +36,6 @@ export default function Support({ auth }) {
 
     const page = usePage();
 
-    console.log(support);
-
     return (
         <AuthenticatedLayout
             url={page.url}
@@ -49,7 +47,7 @@ export default function Support({ auth }) {
             <div className="grid grid-cols-4 gap-4 p-4">
                 <div className="relative dark:bg-[#1F2937] bg-[#DCDCDC] text-white p-6 rounded-xl shadow-[4px_4px_8px_rgba(0,0,0,0.6)]">
                     <h3 className="text-lg text-[#0A0A0A] dark:text-[#FFFFFF] font-semibold">Tempo Resposta:</h3>
-                    <p className="text-3xl dark:text-white text-black font-light mt-2">1.30H</p>
+                    <p className="text-3xl dark:text-white text-black font-light mt-2">1.30h</p>
                 </div>
                 <div className="relative dark:bg-[#1F2937] bg-[#DCDCDC] text-white p-6 rounded-xl shadow-[4px_4px_8px_rgba(0,0,0,0.6)]">
                     <h3 className="text-lg text-[#0A0A0A] dark:text-[#FFFFFF] font-semibold">Pedidos Pendentes:</h3>
