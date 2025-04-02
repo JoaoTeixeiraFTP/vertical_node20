8968e9329cd263153c5056c9492c7fdac04b8f6b
JoaoTeixeiraFTP <joao.teixeira@ftpporto.com>
2025-04-02 11:16:24 +0100

    alteração py-4 sobre o header superior

--- resources/js/Pages/support/document/support-document.jsx
+++ resources/js/Pages/support/document/support-document.jsx
@@ -15,7 +15,7 @@ export default function SupportDocument({ auth, document }) {
                 <Head title="Suporte" />
 
                 <div className="">
-                    <div className="mx-auto px-4 py-2">
+                    <div className="mx-auto px-4 py-4">
                         <div className="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                             <div className="p-6 text-gray-900 dark:text-gray-100">
                                 {document === undefined ? (
